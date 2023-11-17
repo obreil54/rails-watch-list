5881c4623235db0101269f35c0a3c32a8217a475
Ilya Obretetskiy <obreil54@gmail.com>
2023-11-17 13:43:17 +0000

    Created Models and Seeded the Movies Table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+puts 'Destroying Existing Data'
+Movie.destroy_all
+
+puts 'Starting Seed'
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies_data = JSON.parse(movies_serialized)
+movies_data['results'].each do |movie|
+  movie = Movie.new(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average'].round(1)
+  )
+  movie.save
+end
+
+puts 'Seeding Completed'
